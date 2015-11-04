09dd1c116b3a9b6959bc892a4b40b365da587901
ClementEraud <clement.eraud@gmail.com>
2015-11-04 17:33:10 +0100

    Les flux RSS sont là

---
+++ controllers/Ctrl_Clement.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+def aggregator():
+    import gluon.contrib.feedparser as feedparser
+    d = feedparser.parse(
+        "http://www.republicain-lorrain.fr/sports/rss")
+    return dict(title=d.channel.title,
+                link = d.channel.link,
+                description = d.channel.description,
+                created_on = request.now,
+                entries = [
+                  dict(title = entry.title,
+                  link = entry.link,
+                  description = entry.description,
+                  created_on = request.now) for entry in d.entries])
+
+def actu():
+    import gluon.contrib.feedparser as feedparser
+    rssLorrain = aggregator()
+    return locals()
